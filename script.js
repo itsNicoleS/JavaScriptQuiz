4d7220a9b6352ec25989258b2d2e4f0316138c4a
nolli <nolliepolieollie@gmail.com>
2023-06-07 17:59:16 -0700

    modified:   README         modified:   index.HTML         modified:   script.js         modified:   styles.css

--- script.js
+++ script.js
@@ -1,40 +1,150 @@
-eventListener 
-document.getElementById("StartButton").addEventListener("click", startgame); 
-function startgame (){
+ //QUIZ PART 
+$("#StartGame").on("click", startgame); 
+$("#quizPartHere").hide(); 
+$("#highScoresTable").hide();
+$("#high-score-submit-btn").hide(); 
+$("Answer").hide(); 
+$(".answers").on("click", buttonclick);
+$("#high-score-submit-btn").on("click", saveScores); 
 
+var questionIndex;
+var currentQuestion;
+var currentScore;
 
+// TIMER SHIT
+var timer = 90;
+var timerEl = document.getElementById("timerValue");  
+
+function updateTimer() {
+    timer--;
+    timerEl.textContent=timer; 
+    if (timer === 0) {
+        clearInterval(interval);
+    }
 }
 
-/* TIMER CODE */
-let remainingTime= 90;
-const minutes = Math.floor(remainingTimeInSeconds / 60);
-const seconds = remainingTimeInSeconds % 60;
+function startgame () {
+    interval = setInterval(updateTimer, 1000); 
+    $("#StartGame").hide();
+    $("#quizPartHere").show(); 
+    questionIndex = 0;  //display first question 
+    currentScore = 0;
+    currentQuestion = quizQuestions[questionIndex]; 
+    Populate();
+         
+}
+function buttonclick (){
+    // get the value of the button clicked
+    var input = $(this).text();
 
-console.log("Remaining Time: ${minutes} minutes ${seconds} seconds");
+            // compare the value of the button to the currentQuestion.Answer
+    if (input === currentQuestion.Answer)
+    {
+        $("#WereYouRight").text("correct! :D");
+        currentScore = currentScore+10; 
+    }
+    else {
+        $("#WereYouRight").text("you make me sad"); 
+    }
 
-remainingTimeInSeconds =-10;
+    questionIndex++;  
+    
+    if (questionIndex >= quizQuestions.length){   
+        
+        alert("You got score of "+(timer+currentScore)+" !  Good job hoser! ")
+        $("#highScoresTable").show()
+        $("#quizPartHere").hide(); 
+        $("#high-score-submit-btn").show();
+        drawHighScoresTable();
+        clearInterval(interval); 
+    }
 
-const updatedSeconds = remainingTimeInSeconds % 60;
-console.log("Updated Remaining Time: ${updatedMinutes} minutes ${updatedSeconds} seconds"); 
+    else {
+        currentQuestion = quizQuestions[questionIndex]; 
+        Populate(); // Advance the questionIndex and currentquestion to the next question
+    } 
 
-/* HIGH SCORES CODE */
-const playerName = prompt('Congratulations! You achived a high Score! Please to be entering your name:')
-const playerScore = {
-    name: playerName, 
-    score: playerScore
-};
- /*retrieve high scores from local strage */
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []; 
-// Add players score to array
-highScores.push(playerScore);
-// sort scores in descending order
-highScores.sort((a, b) => b.score - a.score); 
-//update high scores
-localStorage.setItem('highScores', JSON.stringify(highScores)); 
-//display high scores to user 
-console.log('High Scores');
-highScores.forEach((score, index) => {
-    ('${index + 1}. ${score.name}: ${score.score}'); 
-}); 
+} 
+       
+function saveScores () {
+    const playerName = prompt('Congratulations, you achieved a high Score! Please to be entering your name:')
+    const playerScore = 
+        {
+            name: playerName, 
+        score: timer
+    };
+    
+    playerScore.score += currentScore;
+    drawHighScoresTable(playerScore);
+        //update high scores
+    localStorage.highScores= JSON.stringify(highScores); 
+        console.log(playerScore.score);
+    
+}
 
+function drawHighScoresTable (newScore){ //====
+    var highScores = []; 
+    if (localStorage.highScores){
+        highScores = JSON.parse(localStorage.highScores);            
+    }
+    if (newScore) {
+    highScores.push(newScore)};
+    
+    highScores.sort((a, b) => b.score - a.score); 
+    $("#highScoresTable tbody").empty();
+    
+        for(let i=0; i<highScores.length; i++) {
 
+        var thisScore = highScores[i]; 
+
+        var newRow = "<tr><td>";
+        
+        newRow += thisScore.name;
+        newRow += "</td><td>"
+        newRow += thisScore.score;
+        newRow += "</td></tr>"
+        $("#highScoresTable tbody").append(newRow); 
+        }; 
+        
+}
+
+function Populate () {
+    $("#Question").text(currentQuestion.Question); 
+    $("#OptionA").text(currentQuestion.Options[0]);
+    $("#OptionB").text(currentQuestion.Options[1]);
+    $("#OptionC").text(currentQuestion.Options[2]);
+    $("#OptionD").text(currentQuestion.Options[3]);
+
+}
+const quizQuestions = 
+[
+    {
+        Question: "Commonly used data types DO NOT include...",
+        Options: ["Booleans", "Alerts", "Numbers","Strings"],
+        Answer: "Alerts"
+       
+    },
+    {
+        Question: "What is the condition of If/Else statements enclosed with?",
+        Options: ["Quotes '\"...\"'", "Curly Braces '{...}'", "Parenthesis '(...)'","Square Brackets '[...]'"],
+        Answer:  "Parenthesis '(...)'" 
+    },
+
+    {
+        Question: "Arrays in JavaScript can be used to store...",
+        Options: ["Numbers and Strings", "Booleans", " Other","All of the Above"],
+        Answer: "All of the Above"
+
+    },
+    {
+        Question: "String values must be surrounded by...",
+        Options:["Quotes '\"...\"'", "Curly Braces '{...}'", "Parenthesis '(...)'","Square Brackets '[...]'"],
+        Answer: "Quotes '\"...\"'"
+
+    },
+    {
+        Question: "A very useful tool for debugging code is...",
+        Options:["javaScript", "Terminal Bash", "Console Log","For Loops"],
+        Answer: "Console Log"
+    }
+];
